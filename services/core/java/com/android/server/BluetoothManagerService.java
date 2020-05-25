657f399c1a0637a9d3dddc738f0698c8f91f47a1
Jagadish Koppula <jkoppula@codeaurora.org>
2020-05-25 13:46:11 +0530

    User switch: Unbind headsetservice in bluetooth Off state.
    Usecase:
    Do user switch continuosly multiple times.
    
    Expectation:
    Auto connect must happen and settings UI should display it.
    
    observed Result:
    Auto-connection on headset profile is success,
    but settings UI is not displaying it as connected.
    
    Root Cause:
    Handler not able to process state change messages in User switch
    scenario, hence bluetoothstatechange call back not updated to
    headset service , headset connection state not reflecting in UI
    
    Fix:
    Made changes in handler to send bluetoothstatechange call back
    in bluetooth off state.
    
    CRs-Fixed: 2685994
    Change-Id: I0fccff89beb29bd1ebf236ef8edfc25cb493f614
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -2603,6 +2603,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                     Slog.d(TAG, "Bluetooth is complete send Service Down");
                 }
                 sendBluetoothServiceDownCallback();
+                sendBluetoothStateCallback(false);
                 unbindAndFinish();
                 sendBleStateChanged(prevState, newState);
 
