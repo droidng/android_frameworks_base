a0bed725a17c236727f4086933f7c30f9c1cc01f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-25 19:15:24 -0300

    BrightnessController: Fix thread exception
    Steps to reproduce:
    
    1. Go to Settings > Display
    2. Enable dark theme and pull down quick settings at same time
    
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: FATAL EXCEPTION: SysUiBg
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: Process: com.android.systemui, PID: 2080
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewRootImpl.checkThread(ViewRootImpl.java:8808)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewRootImpl.focusableViewAvailable(ViewRootImpl.java:4612)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.View.setFlags(View.java:16122)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at android.view.View.setEnabled(View.java:11744)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at com.android.systemui.settings.ToggleSliderView.setEnforcedAdmin(ToggleSliderView.java:101)
    02-25 19:02:31.947  2080  2147 E AndroidRuntime: 	at com.android.systemui.settings.BrightnessController$10.run(BrightnessController.java:432)

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -358,7 +358,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     }
 
     public void checkRestrictionAndSetEnabled() {
-        mBackgroundHandler.post(new Runnable() {
+        mHandler.post(new Runnable() {
             @Override
             public void run() {
                 mControl.setEnforcedAdmin(
