5df9a1fe5987304814770cdbdd27aac380b310c5
nift4 <nift4@protonmail.com>
2022-08-28 14:02:08 +0200

    InputManager: allow to force NULL cursor
    If VNC draws cursor on client side, we do not want to have a second
    cursor visible.
    
    Change-Id: Iab1b9477a9142f7ae23484f76285981bddca1880

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -271,6 +271,7 @@ public class InputManagerService extends IInputManager.Stub
     private final SparseArray<CursorCallbackRecord> mCursorCallbacks = new SparseArray<>();
     private final List<CursorCallbackRecord> mTempCursorCallbacksToNotify =
             new ArrayList<>();
+    private boolean mForceNullCursor = false;
 
     // State for the currently installed input filter.
     final Object mInputFilterLock = new Object();
@@ -496,6 +497,15 @@ public class InputManagerService extends IInputManager.Stub
         synchronized (mLidSwitchLock) {
             mLidSwitchCallbacks.remove(callback);
         }
+    }
+
+    @Override // Binder call
+    public void setForceNullCursor(boolean forceNullCursor) {
+        if (DEBUG) {
+            Slog.d(TAG, "setForceNullCursor: forceNullCursor=" + forceNullCursor + " callingPid="
+                    + Binder.getCallingPid());
+        }
+        mForceNullCursor = forceNullCursor;
     }
 
     @Override // Binder call
@@ -2387,6 +2397,10 @@ public class InputManagerService extends IInputManager.Stub
     @Override
     public void setPointerIconType(int iconId) {
         deliverCursorChanged(iconId, null);
+        if (mForceNullCursor) {
+            nativeSetPointerIconType(mPtr, PointerIcon.TYPE_NULL);
+            return;
+        }
         nativeSetPointerIconType(mPtr, iconId);
     }
 
@@ -2395,6 +2409,10 @@ public class InputManagerService extends IInputManager.Stub
     public void setCustomPointerIcon(PointerIcon icon) {
         Objects.requireNonNull(icon);
         deliverCursorChanged(PointerIcon.TYPE_CUSTOM, icon);
+        if (mForceNullCursor) {
+            nativeSetPointerIconType(mPtr, PointerIcon.TYPE_NULL);
+            return;
+        }
         nativeSetCustomPointerIcon(mPtr, icon);
     }
 
